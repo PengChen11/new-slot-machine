8cad83408d631cc30b5f68920cdb801a3caba716
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-03-24 09:10:10 -0700

    added more functions to the app.js

--- js/app.js
+++ js/app.js
@@ -2,9 +2,11 @@
 
 //imagePool array for holding all gameImage Objects
 var imagePool = [];
+var credits = 100;
+var wager = 0;
 
 //construction function to create image objects
-function gameImage (name, imagePath, wagerMultiplier) {
+function GameImage (name, imagePath, wagerMultiplier) {
   this.name = name;
   this.imagePath = imagePath;
   this.wagerMultiplier = wagerMultiplier;
@@ -12,9 +14,9 @@ function gameImage (name, imagePath, wagerMultiplier) {
 }
 
 //creating gameImage Objects using constructor
-new gameImage('cherry', 'assets/cherry.png', 5);
-new gameImage('seven', 'assets/seven.png', 10);
-new gameImage('gold', 'assets/gold.png', 15);
+new GameImage('cherry', 'assets/cherry.png', 5);
+new GameImage('seven', 'assets/seven.png', 10);
+new GameImage('gold', 'assets/gold.png', 15);
 console.log(imagePool);
 
 //creating variables to access img tags in html
@@ -22,18 +24,80 @@ console.log(imagePool);
 var getImage1 = document.getElementById('img1');
 var getImage2 = document.getElementById('img2');
 var getImage3 = document.getElementById('img3');
+//creating variables to access wager buttons in html
+var getWager1 = document.getElementById('bet1');
+var getWager5 = document.getElementById('bet5');
+var getWager10 = document.getElementById('bet10');
+
+//function to generate random image based on on imagePool
+function randomImageGenerator() {
+  var i = Math.floor(Math.random() * imagePool.length);
+  return imagePool[i];
+}
+
+//function to render new images on to the page.
+function renderGameImage() {
+
+  var newImage1 = randomImageGenerator();
+
+  getImage1.src = newImage1.imagePath;
+  getImage1.name = newImage1.name;
+
+  var newImage2 = randomImageGenerator();
+
+  getImage2.src = newImage2.imagePath;
+  getImage2.name = newImage2.name;
+
+  var newImage3 = randomImageGenerator();
+
+  getImage3.src = newImage3.imagePath;
+  getImage3.name = newImage3.name;
+
+}
+
+//function to update credit balance
+function creditUpdate() {
+
+  //variable to access credit element in html and create a <p> tag
+  var getCredit = document.getElementById('status-dollar');
+  var createParagraph = document.createElement('p');
+
+  //clear previous data and adds new content to P tag
+  getCredit.innerHTML = null;
+  createParagraph.innerHTML = '$' + credits;
+  //adds new message to paragraph and appends to document
+  getCredit.appendChild(createParagraph);
+}
 
 
-//function to generate random images
-
-//function to determinte the value of new random images
-//if a win multiply against wager amount if loss then deducted from credit balance
 
 
-//function to grab value from wager button and start the game function
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //click handler for handling when a water is clicked
+function clickHandler(event) {
+  wager = event.target.value;
+
+  console.log(wager);
+  renderGameImage();
+}
 
 //add listener to html elements for click handler
 //add these to the button wagers
+getWager1.addEventListener('click', clickHandler);
+getWager5.addEventListener('click', clickHandler);
+getWager10.addEventListener('click', clickHandler);
+
 
