f705d36326e424b0d4496ee298906e24be15e230
Peng Chen <pchen0816@outlook.com>
2020-03-25 12:03:56 -0700

    timer fixed. commet fixed

--- js/app.js
+++ js/app.js
@@ -69,7 +69,7 @@ function statusUpdate(status) {
 }
 
 
-
+//function to calculate how much creadit you've earned/lost per spin
 function calculateEarnings(wager) {
   //The output from the spinning wheel is nolonger a img. It is a tag class for the div that holds the ring.
   // spin-1 = carrot;
@@ -82,8 +82,10 @@ function calculateEarnings(wager) {
   var img1 = document.getElementById('ring1').classList.value.slice(5);
   var img2 = document.getElementById('ring2').classList.value.slice(5);
   var img3 = document.getElementById('ring3').classList.value.slice(5);
+  
+  // since we are checking the class tags, we will need to associate that with an image name in the imagePool
   function getName(img){
-    let imageName;
+    var imageName;
     switch (img) {
       case 'spin-1':
         imageName = 'carrot';
@@ -156,7 +158,10 @@ function clickHandler(event) {
   start();
   console.log('clicked');
   var wager = event.target.value;
-  calculateEarnings(wager);
+  // since start function will spin the wheels 3.5s, we set a delay timer for 4s so the result won't pop up before the wheel stops.
+  setTimeout(function (){
+    calculateEarnings(wager);
+  }, 4000);
 }
 
 //add listener to html elements for click handler
@@ -170,33 +175,34 @@ getWager10.addEventListener('click', clickHandler);
 
 
 // Define how many pictures will be used for each ring
-let SLOTS_PER_REEL = 6;
+var SLOTS_PER_REEL = 6;
 
 // var radius = Math.round( ( 116 / 2) / Math.tan( Math.PI / SLOTS_PER_REEL ) );
-let REEL_RADIUS = 106;
+var REEL_RADIUS = 106;  //106 is the number I tested out to make the ring size fit the windows of the slot machine.
 
 //This is the function to create individual rings. we will have 3 rings for the spinning machine;
 function createSlots (ring) {
 
-  let slotAngle = 360 / SLOTS_PER_REEL;
+  var slotAngle = 360 / SLOTS_PER_REEL;
 
-  for (let i = 0; i < SLOTS_PER_REEL; i ++) {
-    let slot = document.createElement('div');
+  //loop 3 times to create 3 rings.
+  for (var i = 0; i < SLOTS_PER_REEL; i ++) {
+    var slot = document.createElement('div');
 
     slot.className = 'slot';
 
     // compute and assign the transform for this slot
-    let transform = 'rotateX(' + (slotAngle * i) + 'deg) translateZ(' + REEL_RADIUS + 'px)';
+    var transform = 'rotateX(' + (slotAngle * i) + 'deg) translateZ(' + REEL_RADIUS + 'px)';
 
     slot.style.transform = transform;
 
-    // setup the number to show inside the slots
-    // the position is randomized to
+    // setup the img to show inside the slots
 
-    let img = document.createElement('img');
+    var img = document.createElement('img');
     img.src = imagePool[i].imagePath;
+    // add the img to the slot
     slot.appendChild(img);
-    // add the poster to the row
+    // add the slots to the ring
     ring.append(slot);
   }
 }
@@ -208,43 +214,41 @@ createSlots(ring2);
 var ring3 = document.getElementById('ring3');
 createSlots(ring3);
 
-
+// just to get a random number between 1-6
 function getSeed() {
-  // generate random number smaller than 6 then floor it to settle between 0 and 5 inclusive
   return Math.ceil(Math.random()*(SLOTS_PER_REEL));
   // return 1;
 }
 
 function spin(timer) {
-  // Since we have 3 rings, we will spin 3 times;
-  for(let i = 1; i < 4; i ++) {
-
-    let lastSeed = 0;
+  // Since we have 3 rings, we will spin one ring at a time, totally 3 times;
+  for(var i = 1; i < 4; i ++) {
+    // if the ring got the same ramdom number comparing to the last spin, ring will not spin, we have to prevent thsi from happening
+    var lastSeed = 0;
 
     // checking that the last seed from the previous iteration is not the same as the current iteration;
-
     // This is to go to the element with ring number ID, grab the value of it's class.
     // lastClass could be shortened to $('#ring'+i).attr('class') if using jQuery;
-    let lastClass = document.getElementById('ring'+i).classList.value;
+    var lastClass = document.getElementById('ring'+i).classList.value;
 
     // since we're giving the ring div individual spin class for animation, at the beginning, there's no spin class, so the length of classList value should be 4, which is 'ring'.
     //but if after we click the spin button, the new class is added to each ring div, trigger the animation, assign a new random spin class to them. Thus the length of that value would be greater than 4.
     if(lastClass.length > 4) {
       lastSeed = parseInt(lastClass.slice(10)); // classList = 'ring spin-' with a number, subtract 10, leaves that number.
     }
-    let seed = getSeed(); // now compair that number with our 1-6 random number
-    while(lastSeed == seed) { // if it matchs, get a new one. Otherwise the ring will not spin
+    var seed = getSeed(); // now compair that number with our 1-6 random number
+    while(lastSeed == seed) { // if it matchs, get a new one. Or the ring will not spin
       seed = getSeed();
     }
 
-    // now everytime click the spin button, the ring number tag will be assigned an animation and the class will be updated accordingly. Details effect could be found in
-    let ring= document.getElementById('ring'+i);
+    // now everytime when click the spin button, the ring number tag will be assigned an animation and the class attr will be updated accordingly. Details effect could be found in CSS file.
+    var ring= document.getElementById('ring'+i);
     ring.style.animation= '1s ease 0s 1 normal none running back-spin, ' + (timer + i*0.5) + 's' + ' ' + 'ease 0s 1 normal none running spin-' + seed;
     ring.className = String('ring spin-' + seed);
 
   }
 }
-
+// function to start the spin
 function start(){
   var timer = 2;
   spin(timer);
